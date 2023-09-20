ec8a068781c165fd40e98fcf83e02a64b18b2c68
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-09-20 10:55:25 -0600

    feature: created layout and navbar

--- app/layout.js
+++ app/layout.js
@@ -19,9 +19,12 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Navigation />
+                {children}
+            </body>
+        </html>
+    )
 }
